5555d404ec4eb3d6ac12037a9b381cc6daa66642
Beatriz Tsumoto <beatriz.tsumoto@aluno.senai.br>
2025-10-27 16:23:24 -0300

    faet: atualiza arquivos

--- src/Models/bruxoModel.js
+++ src/Models/bruxoModel.js
@@ -1,12 +1,12 @@
 // é no model que fazemos a consulta para o banco de dados
 // ex: SELECT * FROM bruxos; porém estamos usando o PRISMA
 // que abstrai o comando SQL
-
 //Importar o prisma Client
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // Crio a variavel findAll e já exporto
+
 export const findAll = async () => {
     // SELECT * FROM bruxos = findMany
     return await prisma.bruxo.findMany({
@@ -14,26 +14,7 @@ export const findAll = async () => {
     });
 }
 
-// Crio a variavel findById e já exporto
-export const findById = async (id) => {
-    // SELECT * FROM bruxos WHERE id = 1;
-    return await prisma.bruxo.findUnique({
-        where: { id: Number(id) }
-    });
-}
 
-//Envia para o banco
-export const create = async (data) => {
-    return await prisma.bruxo.create({
-        data: {
-            nome: data.nome,
-            casa: data.casa,
-            patrono: data.patrono,
-            varinha: data.varinha,
-            anoMatricula: data.anoMatricula
-        }
-    })
-}
 
 export const deleteBruxo = async (id) => {
     return await prisma.bruxo.delete({
@@ -52,6 +33,18 @@ export const update = async (id, data) => {
             ...(data.varinha && { varinha: data.varinha }),
             ...(data.anoMatricula && { anoMatricula: Number(data.anoMatricula) }),
             ...(data.ativo !== undefined && { ativo: data.ativo }),
+        }
+    })
+}
+
+export const create = async (data) => {
+    return await prisma.bruxo.create({
+        data: {
+            nome: data.nome,
+            casa: data.casa,
+            patrono: data.patrono,
+            varinha: data.varinha,
+            anoMatricula: data.anoMatricula
         }
     })
 }
